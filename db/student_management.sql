c691606a02d28880421995666f0feb2faa6fd731
unknown <conanshinichi2000@gmail.com>
2021-11-19 06:53:56 +0700

    second db

--- db/student_management.sql
+++ db/student_management.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th6 27, 2021 lúc 06:20 AM
--- Phiên bản máy phục vụ: 10.4.13-MariaDB
--- Phiên bản PHP: 7.2.32
+-- Thời gian đã tạo: Th10 19, 2021 lúc 12:52 AM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,36 +24,92 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Cấu trúc bảng cho bảng `bangdiem`
+--
+
+CREATE TABLE `bangdiem` (
+  `id` int(11) NOT NULL,
+  `MaSV` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `MaMH` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Diem` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `monhoc`
+--
+
+CREATE TABLE `monhoc` (
+  `id` int(11) NOT NULL,
+  `MaMH` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `TenMH` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `SoTC` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `monhoc`
+--
+
+INSERT INTO `monhoc` (`id`, `MaMH`, `TenMH`, `SoTC`) VALUES
+(1, 'LTCB', 'Lập trình căn bản', 2),
+(2, 'oop', 'Lập trình hướng đối tượng', 4);
+
+-- --------------------------------------------------------
+
+--
 -- Cấu trúc bảng cho bảng `sinhvien`
 --
 
 CREATE TABLE `sinhvien` (
-  `ID` int(11) NOT NULL,
-  `HotenSv` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Gioitinh` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `Email` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `SDT` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `id` int(11) NOT NULL,
+  `HotenSv` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `MaSV` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Gioitinh` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Email` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `SDT` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `sinhvien`
+--
+
+INSERT INTO `sinhvien` (`id`, `HotenSv`, `MaSV`, `Gioitinh`, `Email`, `SDT`) VALUES
+(2, 'Trần Văn B', 'AT1402', 'Nam ', 'btran@gmail.com', '102356');
 
 --
 -- Chỉ mục cho các bảng đã đổ
 --
 
 --
+-- Chỉ mục cho bảng `bangdiem`
+--
+ALTER TABLE `bangdiem`
+  ADD PRIMARY KEY (`MaSV`,`MaMH`),
+  ADD KEY `KN_mh` (`MaMH`);
+
+--
+-- Chỉ mục cho bảng `monhoc`
+--
+ALTER TABLE `monhoc`
+  ADD PRIMARY KEY (`MaMH`);
+
+--
 -- Chỉ mục cho bảng `sinhvien`
 --
 ALTER TABLE `sinhvien`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`MaSV`) USING BTREE;
 
 --
--- AUTO_INCREMENT cho các bảng đã đổ
+-- Các ràng buộc cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT cho bảng `sinhvien`
+-- Các ràng buộc cho bảng `bangdiem`
 --
-ALTER TABLE `sinhvien`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+ALTER TABLE `bangdiem`
+  ADD CONSTRAINT `KN_mh` FOREIGN KEY (`MaMH`) REFERENCES `monhoc` (`MaMH`),
+  ADD CONSTRAINT `KN_sv` FOREIGN KEY (`MaSV`) REFERENCES `sinhvien` (`MaSV`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
